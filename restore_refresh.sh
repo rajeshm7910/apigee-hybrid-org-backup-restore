a39330c6dbeff6b82c0aa4267c1af3c41722ca8a
Rajesh Mishra <rajeshmi@google.com>
2022-08-31 23:02:47 -0700

    restore refresh

--- restore_refresh.sh
+++ restore_refresh.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
 ORG_NAME=$1
-ORG_NAME=${orgname//[-]/_}
+ORG_NAME=${ORG_NAME//[-]/_}
+POD_IP=$2
+CASS_PASSWORD=$3
+
 
 all_keyspaces=$(cqlsh ${CASSANDRA_SEEDS} -u ${APIGEE_DML_USER} -p ${APIGEE_DML_PASSWORD} --ssl  -e 'DESCRIBE KEYSPACES')
+
 for KEYSPACE in $all_keyspaces
 do
         if [[ $KEYSPACE == *$ORG_NAME* ]]; then
-          	echo $KEYSPACE
-          	TABLE_LIST=`cqlsh -e "DESCRIBE KEYSPACE ${KEYSPACE}" | grep "CREATE TABLE" | sed -e 's+CREATE TABLE++' | sed -e 's+(++'`
-          	for table in $TABLE_LIST; do
-          		echo $table
-          		#nodetool refresh -- ${KEYSPACE} ${table}
-          	done
+                  echo $KEYSPACE
+                  TABLE_LIST=$(cqlsh ${CASSANDRA_SEEDS} -u ${APIGEE_DML_USER} -p ${APIGEE_DML_PASSWORD} --ssl -e "DESCRIBE KEYSPACE ${KEYSPACE}" | grep "CREATE TABLE" | sed -e 's+CREATE TABLE++' @
+                  for table in $TABLE_LIST; do
+
+                           table=${table//$KEYSPACE./}
+                           echo "Refreshing " $table
+                           nodetool -h $POD_IP -u cassandra -pw ${CASS_PASSWORD}  refresh -- ${KEYSPACE} ${table}
+
+                  done
         fi
 done
+echo "All Keyspaces Refreshed"
